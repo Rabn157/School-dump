ac2e8892161395ee7ccece584018ff0d5d9fefe5
LazyMexican <franzferman@yahoo.com>
2018-09-07 19:48:53 +0200

    school

---
+++ Week 2/Week 2 Opdracht 18.py
@@ -0,0 +1,7 @@
+var1 = 10
+var2 = 30
+var3 = 2
+
+
+print("dit is a " , (var1 * 8) - (var2 + 32) / var3)
+print("dit is b ", var1 - (6 ** 4) * (var2 ** 3))
---
+++ Week 2/Week 2 Opdracht 19.py
@@ -0,0 +1,8 @@
+var1 = int(input("Vul het eerste getal in: "))
+var2 = int(input("Vul het tweede getal in: "))
+var3 = int(input("Vul het derde getal in: "))
+var4 = int(input("Vul het vierde getal in: "))
+
+print((var1 * var2 * var3) - var4)
+print((var1 * var2) / var3)
+print((var1 ** var2) ** var3)
