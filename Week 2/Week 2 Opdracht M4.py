696ae32943c96113988043188e450797bf789fd6
LazyMexican <franzferman@yahoo.com>
2018-09-07 20:16:04 +0200

    school

--- Week 2/Week 2 Opdracht M4.py
+++ Week 2/Week 2 Opdracht M4.py
@@ -1,6 +1,3 @@
-# Modify the Temperature Conversion program in section 2.4.6 to convert from Celsius to Fahrenheit  instead. 
-# The formula for the conversion is f = (c * 9/5) + 32. 
-
 print('This program will convert degrees Celcius to degrees Fahrenheit')
 
 Celcius = float(input('Enter degrees Celcius: '))
