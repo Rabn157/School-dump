8890679e0e03bf777b36676aa05f3268c82853ac
LazyMexican <franzferman@yahoo.com>
2018-09-11 22:46:25 +0200

    school

--- Week 3/Week 3 Opdracht P4.py
+++ Week 3/Week 3 Opdracht P4.py
@@ -1,17 +1,21 @@
-num1 = (0)
-num2 = (0)
-num3 = (0)
+#input value moet tussen 0 - 100 zitten
+
+
 
 print('The maximum value is 100')
+num1 = float(input('Vul het eerste getal in: '))
 
-while num1 >= 0 and num1 <= 100 and num2 <= 100:
-    num1 = float(input('Enter the first number: '))
-    if num1 <= 100:
-        print('Wrong number')
-    else:
-        num2 = float(input('Enter the second number: '))
-        print('Wrong number')
-    if num2 <= 100:
-        num3 = num1 + num3
-        print(num1, '+', num2,'=',num3,sep='')
+while num1 < 0 or num1 > 100:
+    num1 = float(input('Vul het eerste getal in: '))
+
+num2 = float(input('Vul het tweede getal in: '))
+
+while num2 < 0 or num2 > 100:
+    num2 = float(input('Vul het tweede getal in: '))
+
+num3 = num1 + num2
+
+print(num1, '+', num2, '=', num3, sep='')
+
+
 
